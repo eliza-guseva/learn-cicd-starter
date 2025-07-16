7bdd6ac057f5dcbef67af8a3eb3f7a23ddc3cab1
Eliza Guseva <liza.guseva@hey.com>
2025-07-16 16:18:32 +0200

    checking formatting

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -31,15 +31,15 @@ func TestGetAPIKey(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			key, err := GetAPIKey(tt.headers)
-			
+
 			if err != tt.expectedError {
 				t.Errorf("GetAPIKey() error = %v, expected error %v", err, tt.expectedError)
 				return
 			}
-			
+
 			if key != tt.expectedKey {
 				t.Errorf("GetAPIKey() key = %v, expected key %v", key, tt.expectedKey)
 			}
 		})
 	}
-} 
+}
